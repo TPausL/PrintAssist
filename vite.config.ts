b5f92bc7bb8f6285cd5c1403d2a67e6afe939dca
Timo <coding@tipela.de>
2025-02-25 18:07:32 +0100

    added allowed Hosts to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,8 @@ import { VitePluginNode } from 'vite-plugin-node';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
-
+    server: {
+        //@ts-ignore
+        allowedHosts: ['.tipela.de', '.teckdigital.de'],
+    },
 });
